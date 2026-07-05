221fdb058ed01cd135d5d6d84860de7459fa90db
GforsZi <givaldigumelarsetiawan@gmail.com>
2026-07-05 20:38:18 +0700

    create model user

---
+++ api/internal/model/user.go
@@ -0,0 +1,14 @@
+package model
+
+import (
+	"time"
+)
+
+type User struct {
+	Id        uint      `gorm:"primaryKey" json:"id"`
+	Name      string    `gorm:"size:100;not null" json:"name"`
+	Email     string    `gorm:"size:100;unique;not null" json:"email"`
+	Password  string    `gorm:"not null" json:"-"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
